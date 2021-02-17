63389def0585cc71e58280aa7ff6a7b07d3e145d
Hoang Do <katievincy226@gmail.com>
2021-02-17 08:37:08 -0500

    Add files via upload

---
+++ Math_Operations/Math_Operation.asm
@@ -0,0 +1,30 @@
+; Math_Operations.asm
+;
+; Author:         Hoang Do
+; Student Number: 040961749
+; Date:           22 Feb 2018
+;
+;Purpose:        To Gain Experience with Assembly Language instructions
+
+;Program Constants
+STACK   equ     $2000
+
+        org     $1000           ; Data starts at $1000
+MyArray db      $0C,$81
+Result  ds      2
+
+        org     $2000           ; Program Code starts at $2000
+Start   lds     #STACK          ; Setup the stack
+
+        ldaa    #$25            ; Note: This code is purposely NOT documented
+        ldab    #25
+        adda    MyArray
+        staa    Result
+        stab    Result+1
+        ldaa    Result+1
+        ldab    Result
+        incb
+        ldaa    #%10101010
+        std     Result
+        swi
+        end
